0ec12a644019c77e5afe52b7d8188f125fb1db0a
achyuth reddy <ncsachyuth@gmail.com>
2020-11-28 20:59:05 +0530

    modified a bit

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,14 @@ app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID
 const __dirname = path.resolve() // not present by default if we use esmodule by default
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) =>res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}else{
+    app.get('/', (req, res) => {
+        res.send('api is running')
+    })
+}
 
 //FIXME: why are these two implemented SOLVED
 app.use(notFound)
